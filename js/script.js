020edb8609534affbbd62932639691dd219c1beb
Ryan Cooper <ryangcooper96@gmail.com>
2022-07-23 18:19:05 +0100

    Added chess-pieces folder with piece images. Started looking at calculating feasible moves, and displaying them

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 // function to run once DOM content is loaded
 function init() {
   const chess = {
+    turn: "w",
     selected: undefined,
     pieces: {
-      wP: "♟",
+      wP: { feasibleMoves: [{ row: 0, col: 1 }] },
       wN: 2,
       wB: 3,
       wR: 4,
@@ -17,15 +18,16 @@ function init() {
       bK: 12,
     },
     board: [
-      [["bR"], ["bN"], ["bB"], ["bK"], ["bQ"], ["bB"], ["bN"], ["bR"]],
-      [["bP"], ["bP"], ["bP"], ["bP"], ["bP"], ["bP"], ["bP"], ["bP"]],
-      [[], [], [], [], [], [], [], []],
-      [[], [], [], [], [], [], [], []],
-      [[], [], [], [], [], [], [], []],
-      [[], [], [], [], [], [], [], []],
-      [["wP"], ["wP"], ["wP"], ["wP"], ["wP"], ["wP"], ["wP"], ["wP"]],
-      [["wR"], ["wN"], ["wB"], ["wQ"], ["wK"], ["wB"], ["wN"], ["wR"]],
+      ["bR", "bN", "bB", "bK", "bQ", "bB", "bN", "bR"],
+      ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
+      ["", "", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", "", ""],
+      ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
+      ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"],
     ],
+    pieceLocations: [],
   };
 
   // create board
@@ -60,12 +62,33 @@ function init() {
         // add column and row dataset attributes with respective index values
         square.setAttribute("data-row", i);
         square.setAttribute("data-column", j);
+        // square.setAttribute("data-column", "empty");
         // add event listener
         square.addEventListener("click", function selectSquare(e) {
           console.log(
-            `ROW: ${e.target.dataset.row}, COL: ${e.target.dataset.column}`
+            `ROW: ${e.target.dataset.row}, COL: ${e.target.dataset.column}, PIECE: ${e.target.dataset.piece}`
           );
-          console.log("");
+          chess.pieceLocations.forEach((piece) => {
+            if (piece.row === parseInt(e.target.dataset.row)) {
+              console.log("Yep that's the row!");
+              if (piece.col === parseInt(e.target.dataset.column)) {
+                console.log("Yep that's the column!");
+                document
+                  .querySelector(
+                    `[data-row='${piece.row}'][data-column='${piece.col}']`
+                  )
+                  .classList.add("highlight");
+                piece.moves.forEach((square) => {
+                  document
+                    .querySelector(
+                      `[data-row='${square.row}'][data-column='${square.col}']`
+                    )
+                    .classList.add("highlight");
+                });
+              }
+            }
+          });
+
           // check if square has already been selected.
           // a square has already been selected.
           // a square has NOT been selected.
@@ -81,11 +104,39 @@ function init() {
   function addPieces() {
     for (let i = 0; i < 8; i++) {
       for (let j = 0; j < 8; j++) {
-        document.querySelector(
-          `[data-row='${i}'][data-column='${j}']`
-        ).textContent = chess.board[i][j];
+        if (chess.board[i][j]) {
+          document.querySelector(
+            `[data-row='${i}'][data-column='${j}']`
+          ).innerHTML = `<img width="60px" height="60px" data-row="${i}" data-column="${j}" data-piece="${chess.board[i][j]}" src="./chess-pieces/${chess.board[i][j]}.png">`;
+        }
       }
     }
+  }
+
+  function checkColour() {
+    for (let i = 0; i < 8; i++) {
+      for (let j = 0; j < 8; j++) {
+        console.log(chess.board[i][j]);
+        if (chess.board[i][j].includes(chess.turn)) {
+          chess.pieceLocations.push({
+            row: i,
+            col: j,
+            piece: chess.board[i][j],
+            moves: [],
+          });
+        }
+      }
+    }
+    console.log(chess.pieceLocations);
+  }
+
+  function checkMoves() {
+    chess.pieceLocations.forEach((element) => {
+      if (element.piece.includes("P")) {
+        element.moves.push({ row: element.row - 1, col: element.col });
+        console.log(element);
+      }
+    });
   }
 
   //
@@ -100,6 +151,8 @@ function init() {
 
   createBoardSquares();
   addPieces();
+  checkColour();
+  checkMoves();
 }
 
 window.addEventListener("DOMContentLoaded", init);
